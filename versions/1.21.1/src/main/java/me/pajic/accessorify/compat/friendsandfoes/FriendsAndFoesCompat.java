1da7533b692bad1beddd48613226787e6dd3646c
pajicadvance <pavle.pajic23@gmail.com>
2025-01-09 13:32:47 +0100

    Fix crash when Friends and Foes is not present

--- versions/1.21.1/src/main/java/me/pajic/accessorify/compat/friendsandfoes/FriendsAndFoesCompat.java
+++ versions/1.21.1/src/main/java/me/pajic/accessorify/compat/friendsandfoes/FriendsAndFoesCompat.java
@@ -1,6 +1,9 @@
 package me.pajic.accessorify.compat.friendsandfoes;
 
+import com.faboslav.friendsandfoes.common.init.FriendsAndFoesItems;
 import com.faboslav.friendsandfoes.common.tag.FriendsAndFoesTags;
+import me.pajic.accessorify.util.ModUtil;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 
@@ -8,5 +11,13 @@ public class FriendsAndFoesCompat {
 
     public static boolean isTotem(ItemStack stack) {
         return stack.is(Items.TOTEM_OF_UNDYING) || stack.is(FriendsAndFoesTags.TOTEMS);
+    }
+
+    public static ItemStack getTotemAccessoryStack(Player player) {
+        ItemStack stack = ModUtil.getAccessoryStack(player, FriendsAndFoesItems.TOTEM_OF_FREEZING.get());
+        if (stack.isEmpty()) {
+            stack = ModUtil.getAccessoryStack(player, FriendsAndFoesItems.TOTEM_OF_ILLUSION.get());
+        }
+        return stack;
     }
 }
