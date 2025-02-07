887fd567d0f6f1aa3f9da2486373be0116464259
pajicadvance <pavle.pajic23@gmail.com>
2025-02-07 19:28:09 +0100

    Hotfix 1.21.2+ crash #7

--- src/main/java/me/pajic/accessorify/mixin/MouseHandlerMixin.java
+++ src/main/java/me/pajic/accessorify/mixin/MouseHandlerMixin.java
@@ -1,6 +1,9 @@
 package me.pajic.accessorify.mixin;
 
 import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Local;
 import me.pajic.accessorify.config.ModClientConfig;
 import me.pajic.accessorify.util.ModUtil;
 import net.minecraft.client.Minecraft;
@@ -17,6 +20,7 @@ public class MouseHandlerMixin {
 
     @Shadow @Final private Minecraft minecraft;
 
+    //? if <= 1.21.1 {
     @WrapWithCondition(
             method = "onScroll",
             at = @At(
@@ -37,4 +41,27 @@ public class MouseHandlerMixin {
         }
         return true;
     }
+    //?}
+
+    //? if > 1.21.1 {
+    /*@WrapOperation(
+            method = "onScroll",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/world/entity/player/Inventory;setSelectedHotbarSlot(I)V"
+            )
+    )
+    private void redirectScrollIfScoping(Inventory instance, int selectedHotbarSlot, Operation<Void> original, @Local int i) {
+        if (ModClientConfig.scrollableZoom && minecraft.player != null && ModUtil.shouldScope) {
+            int d = (int) Math.signum(i);
+            if (d != 0) {
+                ModUtil.zoomModifier -= d * (0.1F * ModUtil.zoomModifier);
+                if (ModUtil.zoomModifier > 10) ModUtil.zoomModifier = 10;
+                else if (ModUtil.zoomModifier < 0.1) ModUtil.zoomModifier = 0.1F;
+                else minecraft.player.playSound(SoundEvents.SPYGLASS_STOP_USING);
+            }
+        }
+        else original.call(instance, selectedHotbarSlot);
+    }
+    *///?}
 }
