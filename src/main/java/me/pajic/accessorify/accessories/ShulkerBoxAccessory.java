bc08aba9a51f9649d0a84023066217477152fa78
chililisoup <rebot333gaming@gmail.com>
2025-07-10 13:27:10 -0600

    prevent shulker dupe

--- src/main/java/me/pajic/accessorify/accessories/ShulkerBoxAccessory.java
+++ src/main/java/me/pajic/accessorify/accessories/ShulkerBoxAccessory.java
@@ -3,10 +3,16 @@ package me.pajic.accessorify.accessories;
 import io.wispforest.accessories.api.Accessory;
 import me.pajic.accessorify.util.ModUtil;
 import me.pajic.accessorify.util.MultiVersionUtil;
+import net.minecraft.world.item.ItemStack;
 
 public class ShulkerBoxAccessory implements Accessory {
 
     public static void init() {
         ModUtil.SHULKER_BOXES.forEach(item -> MultiVersionUtil.registerAccessory(item, new ShulkerBoxAccessory()));
+    }
+
+    @Override
+    public int maxStackSize(ItemStack stack){
+        return 1;
     }
 }
